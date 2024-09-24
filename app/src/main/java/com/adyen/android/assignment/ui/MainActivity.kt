c69ebfd4bc50ad1b06548001d31f729e6e5cc7b2
Erik Hoogendoorn <erik.hoogendoorn@gmail.com>
2024-09-24 17:28:47 +0200

    Set MainActivity as @AndroidEntryPoint. Add MainNavHost in MainActivity.

--- app/src/main/java/com/adyen/android/assignment/ui/MainActivity.kt
+++ app/src/main/java/com/adyen/android/assignment/ui/MainActivity.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.adyen.android.assignment.ui.theme.AppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.serialization.Serializable
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +23,17 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             AppTheme {
-                Scaffold { contentPadding ->
-                    Column(modifier = Modifier.padding(contentPadding)) {
-                        Text("Hello World")
-                    }
-                }
+                MainNavHost()
             }
         }
+    }
+}
+
+@Composable
+private fun MainNavHost() {
+    NavHost(
+        navController = rememberNavController(),
+        startDestination = Screen.NearbyPlaces
+    ) {
     }
 }
