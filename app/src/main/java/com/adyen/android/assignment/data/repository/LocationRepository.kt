f45341fc8dcf5f375a81cea0381fc4b2ec911289
Erik Hoogendoorn <erik.hoogendoorn@gmail.com>
2024-09-26 17:53:35 +0200

    Inject FusedLocationProviderClient into LocationRepository directly.

--- app/src/main/java/com/adyen/android/assignment/data/repository/LocationRepository.kt
+++ app/src/main/java/com/adyen/android/assignment/data/repository/LocationRepository.kt
@@ -1,20 +1,19 @@
 package com.adyen.android.assignment.data.repository
 
-import android.content.Context
 import android.location.Location
 import androidx.annotation.RequiresPermission
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
-class LocationRepository @Inject constructor(@ApplicationContext val context: Context) {
-    val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+class LocationRepository @Inject constructor(
+    private val fusedLocationClient: FusedLocationProviderClient
+) {
 
     @RequiresPermission(
         anyOf = ["android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"]
@@ -39,7 +38,6 @@ class LocationRepository @Inject constructor(@ApplicationContext val context: Co
         val locationRequest =
             LocationRequest.Builder(10000)
                 .setMinUpdateDistanceMeters(100F)
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .build()
     }
 }
